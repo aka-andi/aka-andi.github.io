6a1b1f7f24797dcddd38149bde29dfe33e582b34
aka-andi <andrea.motas@gmail.com>
2022-06-21 23:56:41 -0400

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -13,13 +13,7 @@ class About extends Component {
                   develop software that helps people reduce their energy usage. in my free time i enjoy making art, playing instruments, helping out in the
                   community, and exploring the latest in tech.</p>
                   <p>you can reach me at <a href="mailto:amotas2@gmu.edu">amotas2@gmu.edu</a> <br></br>(ﾉ´ヮ´)ﾉ*: ･ﾟ⁺₊</p>
-                  <h3>mentions:</h3>
-                  <ul>
-                     <li><a href="https://cahmp.gmu.edu/2022-cahmp-entrepreneurship-challenge/">center for the advancement of human-machine partnership ⁺₊ entrepreneurship challenge 2022</a></li>
-                     <li><a href="https://kplsorority.org/events/march-convention-2022/ ">kappa phi lambda ⁺₊ national convention 2022 panelists</a></li>
-                     <li><a href="https://cs.gmu.edu/blog/btt-mason-peer-leaders-are-cheer-leaders-for-students/">george mason university, computer science department ⁺₊ break through tech dc article</a></li>
-                     <li><a href="https://computing.gmu.edu/news/2021-10/grace-hoppers-legacy-continues-draw-women-cs ">george mason university, college of engineering and computing ⁺₊ grace hopper 2021 article</a></li>
-                  </ul>
+                  
                </span>
             </div>
 
